ab2d20dc11ad3ab57346b6f6b2810d6166ce9078
zaenal-abidin-syah <aszaenal618@gmail.com>
2025-02-22 00:00:56 +0700

    fixed toggle button?

--- resources/views/layout/partial/headerkegiatan.blade.php
+++ resources/views/layout/partial/headerkegiatan.blade.php
@@ -10,11 +10,11 @@
 
       <div class="justify-end flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0">
         <div class="flex items-center md:ml-auto md:pr-4">
-          <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
+          <div class="relative flex items-center justify-center w-full transition-all rounded-lg ease">
             <button
-                class="flex items-center toggle-button w-14 h-8 rounded-full bg-slate-100 dark:bg-slate-600 relative transition-colors duration-500 ease-infocus:outline-none  focus:border-transparent">
+                class="toggle-button w-14 h-8 rounded-full bg-slate-100 dark:bg-slate-600 relative transition-colors duration-500 ease-infocus:outline-none focus:border-transparent flex items-center">
                   <div id="toggle"
-                    class="rounded-full w-6 h-6 relative ml-2 dark:ml-7 pointer-events-none transition-all duration-300 ease-out">
+                    class="rounded-full w-6 md:h-6 relative ml-2 dark:ml-7 pointer-events-none transition-all duration-300 ease-out">
                     <svg class="block dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                         <path class="fill-slate-300" d="M7 0h2v2H7zM12.88 1.637l1.414 1.415-1.415 1.413-1.413-1.414zM14 7h2v2h-2zM12.95 14.433l-1.414-1.413 1.413-1.415 1.415 1.414zM7 14h2v2H7zM2.98 14.364l-1.413-1.415 1.414-1.414 1.414 1.415zM0 7h2v2H0zM3.05 1.706 4.463 3.12 3.05 4.535 1.636 3.12z" />
                         <path class="fill-slate-400" d="M8 4C5.8 4 4 5.8 4 8s1.8 4 4 4 4-1.8 4-4-1.8-4-4-4Z" />
@@ -26,7 +26,6 @@
                   </div>
             </button>
           </div>
-
         </div>
         <ul class="flex flex-row justify-end pl-0 mb-0 list-none">
           <li class="flex items-center pl-4 xl:hidden">
