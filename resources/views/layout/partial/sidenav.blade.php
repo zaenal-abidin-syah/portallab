ea9f5559eb1fb378ff1a82377c34242f2216ca8d
zaenal-abidin-syah <aszaenal618@gmail.com>
2025-02-28 07:21:01 +0700

    fix dosen

--- resources/views/layout/partial/sidenav.blade.php
+++ resources/views/layout/partial/sidenav.blade.php
@@ -1,5 +1,5 @@
 <!-- sidenav  -->
-<aside class="fixed mt-6 inset-y-0 flex-wrap items-center justify-between block w-full p-0 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
+<aside class="fixed mt-10 inset-y-0 flex-wrap items-center justify-between block w-full p-0 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
   <div class="h-19">
     <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
     <!--<a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="https://demos.creative-tim.com/argon-dashboard-tailwind/pages/dashboard.html" target="_blank">
--- resources/views/layout/partial/sidenav2.blade.php
+++ resources/views/layout/partial/sidenav2.blade.php
@@ -1,12 +1,12 @@
-<aside class="fixed mt-6 inset-y-0 flex-wrap items-center justify-between block w-full p-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
-    <div class="h-19">
-        <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
+<aside class="fixed mt-10 inset-y-0 flex-wrap items-center justify-between block w-full overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-md xl:left-0 xl:translate-x-0" aria-expanded="false">
+    <div class="text-center">
+        <i class="absolute top-0 right-0 p-2 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
         <!--<a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="https://demos.creative-tim.com/argon-dashboard-tailwind/pages/dashboard.html" target="_blank">
             <img src="./assets/img/logo-ct-dark.png" class="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8" alt="main_logo" />
             {{-- <img src="./assets/img/logo-ct.png" class="hidden h-full max-w-full transition-all duration-200 dark:inline ease-nav-brand max-h-8" alt="main_logo" /> --}}
             <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">LAB Tenik Informatika</span>
         </a>-->
-        <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" target="_blank">
+        <a class="block px-2 py-3 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" target="_blank">
             <img src="./assets/img/logo-ct-dark.png" class="inline h-full max-w-full transition-all duration-200 dark:hidden ease-nav-brand max-h-8" alt="main_logo" />
             {{-- <img src="./assets/img/logo-ct.png" class="hidden h-full max-w-full transition-all duration-200 dark:inline ease-nav-brand max-h-8" alt="main_logo" /> --}}
             <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">LAB Teknik Informatika</span>
