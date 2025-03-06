99f11b7b89bc06adf3943db622b8b6ed6684804d
zaenal-abidin-syah <aszaenal618@gmail.com>
2025-03-06 11:07:46 +0700

    fixed darkmode toggle justify to end

--- resources/views/layout/partial/headermain.blade.php
+++ resources/views/layout/partial/headermain.blade.php
@@ -1,5 +1,5 @@
 <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="false">
-    <div class="flex items-center w-full px-4 py-1 mx-auto flex-wrap-inherit">
+    <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
       <nav>
         <!-- breadcrumb -->
         {{-- <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16"> --}}
