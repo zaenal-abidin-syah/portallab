25c0a86d243bf8ac232f74c42f22d61c234a044c
zaenal-abidin-syah <aszaenal618@gmail.com>
2025-04-22 09:56:27 +0700

    delete index in header main and delete comments

--- resources/views/layout/partial/headermain.blade.php
+++ resources/views/layout/partial/headermain.blade.php
@@ -1,7 +1,7 @@
 <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all ease-in shadow-none duration-250 rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="false">
     <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
       <nav>
-        <h6 class="mb-0 font-bold text-white capitalize">Index</h6>
+        {{-- <h6 class="mb-0 font-bold text-white capitalize">Index</h6> --}}
 
       </nav>
 
